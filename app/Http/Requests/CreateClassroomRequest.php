72b46854ab29229b9dcf3baba5d74bcff1f23c0e
João Paulo M <joaopaulopmedeiros@gmail.com>
2021-12-24 11:00:05 -0300

    feat: prevalidation of request

--- app/Http/Requests/CreateClassroomRequest.php
+++ app/Http/Requests/CreateClassroomRequest.php
@@ -25,7 +25,47 @@ class CreateClassroomRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string'
+            'name' => ['required', 'string', 'max:191'],
+            'nickname' => ['required', 'string', 'max:191'],
+            'file' => ['sometimes', 'file', 'max:500', 'mimes:jpeg,png,svg'],
+            'partners.*' => ['email'],
+            'levels' => ['required', 'array'],
+            'levels.*.name' => ['required', 'string'],
+            'levels.*.xp' => ['required','numeric', 'min:0'],
+        ];
+    }
+
+    /**
+     * Custom validation messages.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //classroom name
+            'name.required' => 'O preenchimento do nome da turma é necessário',
+            'name.string' => 'O nome da turma deve estar em formato de texto',
+            'name.max' => 'O nome da turma deve ter no máximo :max caracteres',
+
+            //classroom nickname
+            'nickname.required' => 'O preenchimento do nome da sala é necessário',
+            'nickname.string' => 'O nome nome da sala deve estar em formato de texto',
+            'nickname.max' => 'O nome nome da sala deve ter no máximo :max caracteres',
+
+            //email of partners
+            'partners.*.email' => 'O email não está no formato padrão, corrija-o e tente novamente',
+
+            //levels
+            'levels.required' => 'Deve ser informado ao menos um nível para a turma',
+            'levels.*.xp.min' => 'O xp não deve ser negativo',
+            'levels.*.xp.required' => 'O preenchimento do xp é necessário',
+            'levels.*.name.required' => 'O preenchimento do nome do nível é necessário',
+
+            //banner file
+            'file.file' => 'Apenas arquivos são válidos para neste campo',
+            'file.size' => 'O arquivo deve ter máximo :size mb',
+            'file.mimes' => 'O arquivo deve ter uma das seguintes extensões: :values'
         ];
     }
 }
