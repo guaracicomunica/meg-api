9950859c09ad1ce560ff42f2da1dd996b304c0d9
João Paulo M <joaopaulopmedeiros@gmail.com>
2021-12-24 10:12:53 -0300

    style: invitation mail

--- resources/views/classrooms/invitation.blade.php
+++ resources/views/classrooms/invitation.blade.php
@@ -1,4 +1,7 @@
 <html>
+    <div align="center">
+        <img src="logo.svg"/>
+    </div>
     <p>Olá, você foi convidado a participar da turma {{$name}}.</p>
     <p>Seu código de acesso é {{$code}}.</p>
     <p>Para explorar o mundo encantado da geografia, clique no link abaixo:</p>
