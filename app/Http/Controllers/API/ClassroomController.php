59fcf2c2e5bd4e0c7360d53ba96dfe6b8a26803f
João Paulo M <joaopaulopmedeiros@gmail.com>
2021-12-29 15:04:38 -0300

    refactor: remove unused code

--- app/Http/Controllers/API/ClassroomController.php
+++ app/Http/Controllers/API/ClassroomController.php
@@ -8,11 +8,8 @@ use App\Http\Handlers\GetAllClassroomHandler;
 use App\Http\Handlers\GetParticipantsClassroomHandler;
 use App\Http\Handlers\GetPostsClassroomHandler;
 use App\Http\Handlers\ManageClassroomHandler;
-use App\Http\Requests\CreateActivityRequest;
-use App\Http\Requests\CreateNewsRequest;
 use App\Http\Requests\EnrollClassroomRequest;
 use App\Http\Requests\ManageClassroomRequest;
-use App\Models\Post;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Throwable;
@@ -76,43 +73,5 @@ class ClassroomController extends Controller
     {
         $result = GetParticipantsClassroomHandler::handle($id);
         return response()->json($result);
-    }
-
-    /***
-     * Get all posts of a classroom
-     * @param int $id
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function posts(int $id, Request $request) : JsonResponse
-    {
-        $result = GetPostsClassroomHandler::handle($id, $request);
-        return response()->json($result);
-    }
-
-    /***
-     * @param CreateActivityRequest $request
-     * @return JsonResponse
-     */
-    public function storeActivity(CreateActivityRequest $request) : JsonResponse
-    {
-        Post::create($request->all());
-
-        return response()->json([
-            'message' => 'Activity successfully registered',
-        ],201);
-    }
-
-    /***
-     * @param CreateNewsRequest $request
-     * @return JsonResponse
-     */
-    public function storeNews(CreateNewsRequest $request) : JsonResponse
-    {
-        Post::create($request->all());
-
-        return response()->json([
-            'message' => 'News successfully registered',
-        ], 201);
     }
 }
