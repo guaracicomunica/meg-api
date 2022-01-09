d14c62b11c57e90f76f84c002414f27fdb4a04ad
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-09 19:56:49 -0300

    feat: block view classroom from users who are not member

--- app/Http/Handlers/GetByIdClassroomHandler.php
+++ app/Http/Handlers/GetByIdClassroomHandler.php
@@ -3,11 +3,18 @@
 namespace App\Http\Handlers;
 use App\Http\Resources\ClassroomResource;
 use App\Models\Classroom;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class GetByIdClassroomHandler
 {
     public static function handle(int $classroom_id): ClassroomResource
     {
+        if(!Auth::user()->isMemberOfClassroom($classroom_id))
+        {
+            throw new AccessDeniedHttpException();
+        }
+
         $classroom = Classroom::with(['creator'])->findOrFail($classroom_id);
         return new ClassroomResource($classroom);
     }
