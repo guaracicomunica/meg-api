120f92f8a35cdf3329f2e9d9dab9e17b0d758a20
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-10 21:31:21 -0300

    feat: create topic

--- app/Http/Controllers/API/TopicController.php
+++ app/Http/Controllers/API/TopicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Handlers\CreateTopicHandler;
 
 class TopicController extends Controller
 {
@@ -18,7 +19,7 @@ class TopicController extends Controller
 
     public function store(CreateTopicRequest $request)
     {
-        Cre
+        CreateTopicHandler::handle($request->all());
         return response()->json([
             'message' => 'Topic successfully registered',
         ], 201);
