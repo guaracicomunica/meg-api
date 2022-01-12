85a1e5eea75485752421a0add79a827b55fcf834
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-11 21:43:27 -0300

    feat: (wip) delivery activity

--- app/Http/Handlers/DeliveryActivityHandler.php
+++ app/Http/Handlers/DeliveryActivityHandler.php
@@ -3,6 +3,7 @@
 namespace App\Http\Handlers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class DeliveryActivityHandler
@@ -11,6 +12,7 @@ class DeliveryActivityHandler
     {
         DB::table('users_activities')
             ->where('activity_id', $data['activity_id'])
+            ->where('user_id', Auth::user()->id)
             ->update(['delivered_at' => Carbon::now()]);
 
         //upload de arquivos
