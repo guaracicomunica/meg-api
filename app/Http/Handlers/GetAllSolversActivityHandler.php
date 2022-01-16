5ba051f7a640ec93fab191a5bdbe4c69371a50d5
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-15 23:59:53 -0300

    feat: format when there are no solvers

--- app/Http/Handlers/GetAllSolversActivityHandler.php
+++ app/Http/Handlers/GetAllSolversActivityHandler.php
@@ -21,20 +21,26 @@ class GetAllSolversActivityHandler
             ->where('activity_id', $request->get('activity_id'))
             ->paginate($request->get('per_page'));
 
-        if(count($records) == 0)
-        {
-            return [];
-        }
-
         $totalDeliveredActivities = UserActivity::where('activity_id', $request->get('activity_id'))->whereNotNull('delivered_at')->count();
         $totalAssignments = UserActivity::where('activity_id', $request->get('activity_id'))->count();
 
-        return [
-            'totalDeliveredActivities' => $totalDeliveredActivities,
-            'totalAssignments' => $totalAssignments,
-            'classroom' => $records->first()->activity->post->classroom->name,
-            'deadline' => $records->first()->activity->deadline,
-            'users' => SolverResource::collection($records)
-        ];
+        if($totalAssignments == 0)
+        {
+            return [
+                'totalDeliveredActivities' => $totalDeliveredActivities,
+                'totalAssignments' => $totalAssignments,
+                'classroom' => null,
+                'deadline' => null,
+                'users' => []
+            ];
+        } else {
+            return [
+                'totalDeliveredActivities' => $totalDeliveredActivities,
+                'totalAssignments' => $totalAssignments,
+                'classroom' => $records->first()->activity->post->classroom->name,
+                'deadline' => $records->first()->activity->deadline,
+                'users' => SolverResource::collection($records)
+            ];
+        }
     }
 }
