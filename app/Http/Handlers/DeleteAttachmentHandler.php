40a9999c3d9cf250d773244ba4788dd35d22e8f3
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-18 22:20:56 -0300

    feat: exclude post attachment

--- app/Http/Handlers/DeleteAttachmentHandler.php
+++ app/Http/Handlers/DeleteAttachmentHandler.php
@@ -3,15 +3,33 @@
 namespace App\Http\Handlers;
 
 use App\Models\PostAttachment;
+use App\Utils\File;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DeleteAttachmentHandler
 {
 
+    /***
+     * @param $id
+     * @return void
+     */
     public static function handle($id)
     {
-        PostAttachment::whereHas('post', function($query){
-            $query->where('posts.creator_id', Auth::user()->id);
-        })->findOrFail($id)->delete();
+        try {
+            DB::beginTransaction();
+
+            $attachment = PostAttachment::whereHas('post', function($query){
+                $query->where('posts.creator_id', Auth::user()->id);
+            })->findOrFail($id);
+
+            File::delete($attachment->path);
+
+            DB::commit();
+        } catch (Exception $ex)
+        {
+            DB::rollBack();
+            throw $ex;
+        }
     }
 }
