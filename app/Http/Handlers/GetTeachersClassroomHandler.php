dbbc1b6160e68ed64f4b55a21415c26327878e48
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-26 21:52:19 -0300

    fix: denie access to users who are not members of classroom

--- app/Http/Handlers/GetTeachersClassroomHandler.php
+++ app/Http/Handlers/GetTeachersClassroomHandler.php
@@ -4,11 +4,16 @@ namespace App\Http\Handlers;
 
 use App\Http\Resources\TeacherResource;
 use App\Models\Classroom;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class GetTeachersClassroomHandler
 {
     public static function handle(int $id)
     {
+        if(!Auth::user()->isMemberOfClassroom($id)) {
+            throw new AccessDeniedHttpException();
+        }
         $classroom = Classroom::findOrFail($id);
         $students = $classroom->teachers()->paginate();
         return TeacherResource::collection($students)->response()->getData();
